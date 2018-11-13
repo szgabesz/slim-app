4ff4e6b5dd035517ce8c9c439bd0f2e67141445a
gabron <szgabesz9102@gamil.com>
2018-11-13 22:21:19 +0100

    Add exception handler

---
+++ src/handlers/exception.php
@@ -0,0 +1,17 @@
+<?php 
+// namespace AppExceptions;  
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
+class Handler implements ExceptionHandler {  
+  public function report(Exception $e) {
+    throw $e;
+  }
+
+  public function render($request, Exception $e) {
+    throw $e;
+  }
+
+  public function renderForConsole($output, Exception $e) {
+    throw $e;
+  }
+}
